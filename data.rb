83ee0d086b8f41f4a5e5a5b1cdb3bd1d64613cc9
kpy <kate@drkatewolin.com>
2013-10-15 14:54:16 -0500

    first commit

--- data.rb
+++ data.rb
@@ -4,16 +4,32 @@ puts "\nLoading data for the previous TSL students.. "
 
 load 'seeds.rb'
 
-STUDENTS.each do |name|
-  puts "#{name}"
-end
+# STUDENTS.each do |name|
+#   puts "#{name}"
+# end
 
-
+# puts STUDENTS.class #ARRAY
+# puts STUDENTS.count #173 students
 
 # CHALLENGE 1
 
 # Using the each method and the STUDENTS array. Find Vince
 # and return the following output:
+
+# vince=STUDENTS.collect { |vince| vince["last_name"] == "Cabansag" }
+
+#MY SOLUTION
+
+# STUDENTS.each do |search|
+# # if search.collect  { |vince| vince["last_name"] == "Cabansag" }
+# if search["last_name"] == "Cabansag"
+#   puts "name: #{search["first_name"]} #{search['last_name']}
+#   twitter: @#{search['twitter']}
+#   github: #{search['github']}
+#   blog: http://#{search['blog']}
+#   email: #{search['email']}"
+# end
+# end
 
 # name: Vince Cabansag
 # twitter: @vcabansag
@@ -26,6 +42,9 @@ end
 
 # Gather all of the Twitter handles into an array and show them on separate lines
 
+# #MY SOLUTION
+# twitter_handles = STUDENTS.collect { |student| "@#{student["twitter"]}" }
+# twitter_handles.each { |handle| puts "#{handle}"}
 
 
 # CHALLENGE 3
@@ -33,6 +52,12 @@ end
 # Update your script so that you only show values of a person if they're present
 # We don't want to see "blog: " or "github: "
 
+twitter_handles = STUDENTS.collect { |student| "#{student["twitter"]}" }
+
+new_twitter_handles=[ ]
+new_twitter_handles = twitter_handles.compact
+# puts new_twitter_handles.class
+new_twitter_handles.each { |handle| puts "twitter: @#{handle}"}
 
 
 # CHALLENGE 4
